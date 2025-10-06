6712ab1a84977c04b6eb95938c0cbf025ab8a623
Timur <yen.tima396@bk.ru>
2025-10-06 12:16:15 +0500

    second commit

--- assets/js/script_page1.js
+++ assets/js/script_page1.js
@@ -33,3 +33,67 @@ document.getElementById("button-search").addEventListener("click", () => {
 function indicators() {
     document.getElementsByClassName("circle")[0].style.backgroundColor = "rgb(134, 0, 0)";
 }
+
+
+
+
+
+
+
+
+const slider = document.getElementById("slider");
+    const currentValue = document.getElementById("currentValue");
+
+    function formatNumber(num) {
+      return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+    }
+
+    slider.addEventListener("input", () => {
+      currentValue.textContent = formatNumber(slider.value);
+    });
+
+
+
+
+  function updateSliderFill() {
+    const min = Number(slider.min);
+    const max = Number(slider.max);
+    const val = Number(slider.value);
+    const percent = ((val - min) / (max - min)) * 100;
+
+    slider.style.background = `linear-gradient(to right, red ${percent}%, #ddd ${percent}%)`;
+  }
+
+  slider.addEventListener("input", updateSliderFill);
+  updateSliderFill(); // при загрузке страницы
+
+
+    const todayInput = document.getElementById('today');
+    const today = new Date();
+    const yyyy = today.getFullYear();
+    const mm = String(today.getMonth() + 1).padStart(2, '0');
+    const dd = String(today.getDate()).padStart(2, '0');
+    todayInput.value = `${yyyy}-${mm}-${dd}`;
+
+
+    document.getElementById("search-result").addEventListener("click", () => {
+        const result = document.getElementById("result");
+        if(result.style.display == "flex") {
+            result.style.display = "none";
+        }
+
+        else {
+            result.style.display = "flex";
+        }
+    });
+
+    document.getElementById("button-next").addEventListener("click", () => {
+        const kad = document.getElementById("kad");
+        if(kad.style.display == "flex") {
+            kad.style.display = "none";
+        }
+
+        else {
+            kad.style.display = "flex";
+        }
+    });
